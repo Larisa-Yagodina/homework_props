a0ad89b3a344c692f6e5695f5867d94eda474b4b
Larisa-Yagodina <cheeseroll@mail.ru>
2021-01-03 16:31:05 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import MainMenu from "./MainMenu";
+import QuestionAndAnswers from "./QuestionAndAnswers";
+import Services from "./Services";
+import Contacts from "./Contucts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const companyName = 'Travel Agency';
+
+    const mainMenu = ['Homepage', 'Services', 'Q&A', 'Contacts'];
+
+    const services = [
+        {place: 'Hawaii', hotel: 'Marriott', term: '8 days'},
+        {place: 'Maldives', hotel: 'Four Season', term: '10 days'},
+        {place: 'Orlando', hotel: 'Sheraton', term: '7 days'},
+    ]
+
+    const questionsAndAnswers = [
+        {question: 'How to choose dates?', answer: 'See weather statistics.',},
+        {question: 'What activity we can choose?', answer: 'You can choose snorkeling, surfing or Theme Parks.',},
+    ]
+
+    const contacts = {
+        address: 'Los Angeles, Beverly Hills, 103',
+        phoneNumber: '+7888777666',
+        email: 'adventures@gmail.com'
+    }
+
+    return (
+        <div className={'main'}>
+            <h1>{companyName}</h1>
+            <p>
+                {mainMenu.map(el => <MainMenu menuItem={el}/>)}
+            </p>
+            <Services services={services}/>
+            <QuestionAndAnswers questionsAndAnswers={questionsAndAnswers}/>
+            <Contacts contacts={contacts}/>
+        </div>
+    );
 }
 
 export default App;
